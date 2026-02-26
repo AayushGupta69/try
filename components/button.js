546d09767d05934b9f91bc0163f0027d3eafbea9
Aayush Gupta <aayushguptaworks@gmail.com>
2026-02-26 21:50:31 +0530

    Added button.js component

---
+++ components/button.js
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+
+const Button = ({ text, className, id }) => {
+  return (
+    <a
+      onClick={(e) => {
+        e.preventDefault();
+
+        const target = document.getElementById("counter");
+
+        if (target && id) {
+          const offset = window.innerHeight * 0.15;
+
+          const top =
+            target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+          window.scrollTo({ top, behavior: "smooth" });
+        }
+      }}
+      className={`${className ?? ""} cta-wrapper`}
+    >
+      <div className="cta-button group">
+        <div className="bg-circle" />
+        <p className="text">{text}</p>
+        <div className="arrow-wrapper">
+          <Image src="/arrow-down.svg" alt="arrow" width={18} height={18} />
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default Button;
