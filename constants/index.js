2eb8d0e4065237be251365d38065a61c3891bc90
Aayush Gupta <aayushguptaworks@gmail.com>
2026-02-26 21:57:07 +0530

    Added counterItems

--- constants/index.js
+++ constants/index.js
@@ -27,3 +27,10 @@ export const words = [
   { text: "Designs", imgPath: "/designs.svg" },
   { text: "Code", imgPath: "/code.svg" },
 ];
+
+export const counterItems = [
+  { value: 1, suffix: "+", label: "Years of Internship Experience" },
+  { value: 100, suffix: "+", label: "Bugs Resolved" },
+  { value: 7, suffix: "+", label: "Techs Mastered" },
+  { value: 95, suffix: "%+", label: "Stability Achieved" },
+];
