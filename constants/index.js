17cb001880b269e00afb37c8e8d45496444468f3
Aayush Gupta <aayushguptaworks@gmail.com>
2026-02-27 12:37:56 +0530

    Added testimonials and assets

--- constants/index.js
+++ constants/index.js
@@ -74,3 +74,34 @@ export const socialLinks = [
     label: "X",
   },
 ];
+
+export const testimonials = [
+  {
+    name: "Prof. Ganesh Ramakrishnan",
+    position: "CSE Department, Indian Institute of Technology Bombay",
+    review:
+      "Aayush was an invaluable member of our team during his twelve-month internship at IIT Bombay, where he successfully developed and maintained the LEAP-PE Tool, a desktop application for post-editing machine-translated documents. He effectively incorporated new features, resolved bugs, and conducted user research. Aayush demonstrated exceptional achievements, showcasing strong team management, problem-solving skills, and the ability to quickly learn new technologies like QT. His technical expertise, commitment to quality, and effective communication skills make him an ideal candidate for future opportunities in software development.",
+    imgPath: "/ganesh-r.jpg",
+  },
+  {
+    name: "Sadam Hussain",
+    position: "Software Engineer at Oracle",
+    review:
+      "Aayush consistently demonstrated exceptional dedication and efficiency, ensuring all tasks were completed promptly. Aayush is a true team player and brings great value to any project he is involved in.",
+    imgPath: "/sadam-h.jpg",
+  },
+  {
+    name: "Nishant Wankhede",
+    position: "M.Tech student at IIT Delhi",
+    review:
+      "Throughout his internship, Aayush has consistently demonstrated exceptional timeliness and efficiency. His keen understanding of task urgency has resulted in swift and reliable deliveries. Aayush possesses a remarkable ability to quickly grasp new feature workflows, allowing him to contribute meaningfully. His cooperative nature and proactive approach have been invaluable assets to our team. Based on his performance, I wholeheartedly recommend Aayush for software development roles. I am confident that he will bring significant value to any position he undertakes, leveraging his technical competence, adaptability, and commitment to excellence.",
+    imgPath: "/nishant-w.jpg",
+  },
+  {
+    name: "Sagar Vats",
+    position: "CTO Multyfi",
+    review:
+      "I highly recommend Aayush for his exceptional technical expertise and innovative approach to software development. He has consistently demonstrated the ability to design and implement scalable, efficient, and user-friendly solutions, resulting in significant improvements in performance and productivity. Aayush's strong problem-solving skills, attention to detail, and commitment to excellence make him a valuable asset to any organization. He is a collaborative team player with excellent communication skills, able to effectively translate complex technical concepts into actionable insights. I strongly endorse Aayush for any opportunity, and I am confident he will continue to drive innovation and deliver outstanding results.",
+    imgPath: "/sagar-v.jpg",
+  },
+];
